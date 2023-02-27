4cde2d2f5dae66372c723afd0bcc2ffd737a4890
Savo Bajic <bajic.savo@gmail.com>
2023-02-27 01:16:00 -0500

    Added default state for all parameters

--- userSettingsObjects.py
+++ userSettingsObjects.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List
 
 # Generic dataclass for any value that may have an associated increment and limits for both.
@@ -44,179 +44,146 @@ class plottingSettings:
 
 @dataclass
 class codingGainSettings:
-    gain: valueWithLimits
     addCoding: bool = False
+    gain: valueWithLimits = valueWithLimits()
     
 
 @dataclass
 class generalSettings:
-    symbolRate: valueWithLimits #symbol rate [S/s] (or 2x frequency [Hz])
+    symbolRate: valueWithLimits = valueWithLimits() #symbol rate [S/s] (or 2x frequency [Hz])
         
     # Signaling mode ('standard','1+D','1+0.5D','clock')
-    signalingMode: str
+    signalingMode: str = 'standard'
     
     # Coding gain
-    codingGain: codingGainSettings
+    codingGain: codingGainSettings = codingGainSettings()
     
     # Modulation (M-PAM)
-    modulation: valueWithLimits
+    modulation: valueWithLimits = valueWithLimits()
     
     # Resolution
-    samplesPerSymb: valueWithLimits # horizontal resolution
-    yAxisLength: valueWithLimits    # vertical resolution (must be odd)
+    samplesPerSymb: valueWithLimits = valueWithLimits() # horizontal resolution
+    yAxisLength: valueWithLimits = valueWithLimits()    # vertical resolution (must be odd)
     
     # General display
-    numbSymb: valueWithLimits    # number of symbols to plot
-    contLevels: valueWithLimits # number of contour levels
+    numbSymb: valueWithLimits = valueWithLimits()    # number of symbols to plot
+    contLevels: valueWithLimits = valueWithLimits() # number of contour levels
         
     # Target BER
-    targetBER: valueWithLimits # used for measurement purposes
+    targetBER: valueWithLimits = valueWithLimits() # used for measurement purposes
 
-    plotting: plottingSettings
+    plotting: plottingSettings = plottingSettings()
 
 @dataclass
 class adaptionSettings:
 
-    # Adaption complexity
-    totalParents: valueWithLimits
-    childrenPerParent: valueWithLimits
-    totalMutations: valueWithLimits
-    mode1Generations: valueWithLimits # coarse adjustment
-    mode2Generations: valueWithLimits # fine adjustment
-
-    knobs: List[str]
-
     adapt: bool = False
+    knobs: list = field(default_factory=lambda : [])
+
+    # Adaption complexity
+    totalParents: valueWithLimits = valueWithLimits()
+    childrenPerParent: valueWithLimits = valueWithLimits()
+    totalMutations: valueWithLimits = valueWithLimits()
+    mode1Generations: valueWithLimits = valueWithLimits()# coarse adjustment
+    mode2Generations: valueWithLimits = valueWithLimits() # fine adjustment
 
 
 @dataclass
 class distortionSetting:
-    fileName: str
     addDistortion: bool = False
+    fileName: str = ''
     
 
 @dataclass
 class noiseSettings:
-    stdDeviation: valueWithLimits   # TX random noise standard diviation [V]
-    amplitude: valueWithLimits      # TX deterministic noise amplitude [V]
-    frequency: valueWithLimits      # TX deterministic noise frequency [Hz]
-    noiseDensity: valueWithLimits   # Noise density [V^2/Hz]
     addNoise: bool = False
+    stdDeviation: valueWithLimits = valueWithLimits()   # TX random noise standard diviation [V]
+    amplitude: valueWithLimits = valueWithLimits()      # TX deterministic noise amplitude [V]
+    frequency: valueWithLimits = valueWithLimits()      # TX deterministic noise frequency [Hz]
+    noiseDensity: valueWithLimits = valueWithLimits()   # Noise density [V^2/Hz]
 
 @dataclass
 class jitterSettings:
-    stdDeviation: valueWithLimits # TX random jitter standard diviation [UI]
-    amplitude: valueWithLimits    # TX deterministic jitter amplitude [UI]
-    DCD: valueWithLimits          # TX duty-cycle distortion jitter [UI]
     addJitter: bool = False
+    stdDeviation: valueWithLimits = valueWithLimits() # TX random jitter standard diviation [UI]
+    amplitude: valueWithLimits = valueWithLimits()    # TX deterministic jitter amplitude [UI]
+    DCD: valueWithLimits = valueWithLimits()          # TX duty-cycle distortion jitter [UI]
 
 @dataclass
 class equalizerSettings:
-    preTaps: List[valueWithLimits]
-    mainTap: valueWithLimits 
-    postTaps: List[valueWithLimits]
     addEqualization: bool = False
+    preTaps: list = field(default_factory=lambda : [])
+    mainTap: valueWithLimits = valueWithLimits() 
+    postTaps: list = field(default_factory=lambda : [])
 
 
 @dataclass
 class transmitterSettings:
-    # Maximum amplitude [V]
-    signalAmplitude: valueWithLimits
     
-    # Rise/fall time [s]
-    tRise: valueWithLimits
-    
-    # TX bandwidth [Hz]
-    TXBandwidth: valueWithLimits
+    signalAmplitude: valueWithLimits = valueWithLimits() # Maximum amplitude [V]
+    TXBandwidth: valueWithLimits = valueWithLimits() # TX bandwidth [Hz]
     
     # Impulse cursor length
-    preCursorCount: valueWithLimits 
-    postCursorCount: valueWithLimits
+    preCursorCount: valueWithLimits = valueWithLimits()
+    postCursorCount: valueWithLimits = valueWithLimits()
+    tRise: valueWithLimits = valueWithLimits() # Rise/fall time [s]
 
-    # Pre-emphasis
-    EQ: equalizerSettings
-
-    # Jitter
-    jitter: jitterSettings
-    
-    # Noise
-    noise: noiseSettings
-
-    # Distortion
-    distortion: distortionSetting
-
-    # Add source impedance (drop amplitude by half)
-    includeSourceImpedance: bool = True
+    EQ: equalizerSettings = equalizerSettings() # Pre-emphasis
+    jitter: jitterSettings = jitterSettings()
+    noise: noiseSettings = noiseSettings()
+    distortion: distortionSetting = distortionSetting()
+    includeSourceImpedance: bool = True # Add source impedance (drop amplitude by half)
 
 @dataclass
 class preAmpSettings:
-    gain: valueWithLimits
     addGain: bool = False
+    gain: valueWithLimits = valueWithLimits()
 
 @dataclass
 class CTLESettings: 
-    zeroFreq: valueWithLimits       # frequency of first zero [Hz]
-    zeroNumb: valueWithLimits       # number of zeros
-    pole1Freq: valueWithLimits      # frequency of first pole [Hz]
-    pole1Numb: valueWithLimits      # number of first poles
-    pole2Freq: valueWithLimits      # frequency of additional poles [Hz]
-    pole2Numb: valueWithLimits      # number of additional poles
+    zeroFreq: valueWithLimits = valueWithLimits()      # frequency of first zero [Hz]
+    zeroNumb: valueWithLimits = valueWithLimits()       # number of zeros
+    pole1Freq: valueWithLimits = valueWithLimits()      # frequency of first pole [Hz]
+    pole1Numb: valueWithLimits = valueWithLimits()      # number of first poles
+    pole2Freq: valueWithLimits = valueWithLimits()      # frequency of additional poles [Hz]
+    pole2Numb: valueWithLimits = valueWithLimits()      # number of additional poles
     addEqualization: bool = False   
 
 @dataclass
 class receiverSettings:
-    # Maximum amplitude [V]
-    signalAmplitude: valueWithLimits
     
-    # PreAmp
-    preAmp: preAmpSettings
-
-    # CTLE
-    CTLE: CTLESettings
-    
-    # FFE
-    FFE: equalizerSettings
-    
-    # DFE
-    DFE: equalizerSettings
-    
-    # Jitter
-    jitter: jitterSettings
-    
-    # Noise
-    noise: noiseSettings
-    
-    # Distortion
-    distortion: distortionSetting
+    signalAmplitude: valueWithLimits = valueWithLimits() # Maximum amplitude [V]
+    preAmp: preAmpSettings = preAmpSettings()
+    CTLE: CTLESettings = CTLESettings()
+    FFE: equalizerSettings = equalizerSettings()
+    DFE: equalizerSettings = equalizerSettings()
+    jitter: jitterSettings = jitterSettings()
+    noise: noiseSettings = noiseSettings()
+    distortion: distortionSetting = distortionSetting()
 
 
 @dataclass
 class channelSettings:
-        # Channel file names (ensure channel data has same frequency points)
-    fileNameThru: str
-    fileNamesNEXT: List[str]
-    fileNamesFEXT: List[str]
+
+    # Apply channel/cross-talk
+    addChannel: bool = True
+    addCrossTalk: bool = True
 
     # Noise
-    noise: noiseSettings
+    noise: noiseSettings = noiseSettings()
 
-    overrideFileName: str 
-
-    modelCircuitTFName: str
-    
-
-    
     # Add notch (must update transfer function)
-    notchFreq: valueWithLimits      # frequency of notch
-    notchAttenuation: valueWithLimits # attenuation at notch [dB]
+    notchFreq: valueWithLimits = valueWithLimits()     # frequency of notch
+    notchAttenuation: valueWithLimits = valueWithLimits() # attenuation at notch [dB]
     addNotch: bool = False
        
     # Convolve pulse response (convolve all channels with additional transfer function, must update transfer function)
     modelCircuitTF: bool = False
+    modelCircuitTFName: str = ''
 
     # Override channel pulse response (must have same over-sampling frequency)
     overrideResponse: bool = False
-    
+    overrideFileName: str = ''
     
     # Approximate cross-talk to speed up simulation
     approximate: bool = True
@@ -224,17 +191,17 @@ class channelSettings:
     # Make cross-talk channels asynchronous
     makeAsynchronous: bool = True
 
-    # Apply channel/cross-talk
-    addChannel: bool = True
-    addCrossTalk: bool = True
-
+    # Channel file names (ensure channel data has same frequency points)
+    fileNameThru: str = ''
+    fileNamesNEXT: list = field(default_factory=lambda : [])
+    fileNamesFEXT: list = field(default_factory=lambda : [])
 
 
 # The complete compiled class for all simulation settings
 @dataclass
 class simulationSettings:
-    general: generalSettings
-    adaption: adaptionSettings
-    transmitter: transmitterSettings
-    channel: channelSettings
-    receiver: receiverSettings
+    general: generalSettings = generalSettings()
+    adaption: adaptionSettings = adaptionSettings()
+    transmitter: transmitterSettings = transmitterSettings()
+    channel: channelSettings = channelSettings()
+    receiver: receiverSettings = receiverSettings()
