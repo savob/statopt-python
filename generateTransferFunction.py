Fixed notch phase to be in radians

generateTransferFunction.py
@@ -203,9 +203,9 @@ def loadFiles(simSettings: simulationSettings, simResults: simulationStatus):
             k = 2 * np.pi * notchFreq / 10
             g = 10 ^ (notchAttenuation / 20)
             notchLTI = ml.tf([1, 5*k/g, 100*k^2], [1, 5*k, 100*k^2])
-            mag, phase, w = ml.bode(notchLTI, 2*np.pi*freqs, plot=False)
+            mag, phase, w = ml.bode(notchLTI, 2*np.pi*freqs, plot=False, deg=False) # Return phase in radians
             mag = np.squeeze(mag)
-            phase = np.squeeze(phase) * 180 / np.pi
+            phase = np.squeeze(phase)
             notchTF = mag * np.exp(np.pi * phase)
             tranFunc = tranFunc * notchTF
 